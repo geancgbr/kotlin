dd18f23b300e5b3b305e80b0aad9fd5827bba29f
geancarlos <geangbr@gmail.com>
2026-03-07 22:14:44 -0300

    Variables, Primitive Types, String Templates, and Functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,32 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    val nome = "Gean"
+    val idade = 25
+    val pi: Double = 3.14
+    val x: String? = null
+
+    val i: Int = 10
+    val d: Double = 3.14
+    val f: Float = 3.14f
+    val l: Long = 100L
+    val b: Boolean = true
+    val c: Char = 'A'
+    val s: String = "Gean"
+
+    val name = "Gean"
+    println("Olá, $name")
+    println("2 + 2 = ${2 + 2}")
+
+    fun soma(a: Int, b: Int): Int {
+        return a + b
     }
+
+    fun soma2(a: Int, b: Int) = a + b
+
+    fun greet(name: String = "mundo") = "Olá, $name!"
+
+    fun log(msg: String): Unit = println(msg)
+
 }
